4322c209cc12d0a7e616b81cf48eaf9e84eb8e6a
Fabiola <fabiola.aurelien@gmail.com>
2026-04-26 10:46:23 -0400

    Added a data folder to house array of tasklist items for testing purposes

--- taskManager/src/App.tsx
+++ taskManager/src/App.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
-
 import './App.css'
+import tasksData from '../data/tasks.json';
+import type { Task } from '../types';
+
+const tasks: Task[] = tasksData;
+
+
 
 function App() {
  
